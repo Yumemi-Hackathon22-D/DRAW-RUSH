6dfd80f578a51129a81f525f5663eabe62a3771e
tosup_msi <j20440@inc.kisarazu.ac.jp>
2022-05-01 20:36:32 +0900

    firebaseにつながらないので涙が止まりません。

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,18 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import db from './../firebase/index';
+import { collection, doc, setDoc, getDocs } from 'firebase/firestore';
+import { TextField } from '@mui/material';
 
-function Chat() {
-  const addChat = () => {
-    const chatName = prompt('チャットルーム名を入力してくだせえ');
+const JoinChat = (props) => {
+  var rooms = collection(db, 'rooms');
+  let roomRef;
+  const [roomName, setroomName] = useState('');
 
-    if (chatName) {
-      db.collection('rooms').add({
-        name: chatName,
-      });
-    }
+  let joinRoom = {
+    Name: roomName,
   };
 
-  return;
-}
+  roomRef = setDoc(rooms, joinRoom);
 
-export default Chat;
+  return (
+    <div>
+      <TextField
+        value={roomName}
+        onChange={(e) => {
+          setroomName(e.target.value);
+        }}
+        label='ルーム名'
+        variant='filled'
+      ></TextField>
+      <button onClick={JoinChat()}>Join</button>
+    </div>
+  );
+};
+
+export default JoinChat;
+
+// export default class joinRoom extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     var rooms = collection(db, 'rooms');
+//     this.state = {
+//       roomName: '',
+//     };
+//   }
+
+//   joinRoom = (props) => {
+//     let roomRef;
+
+//     if (!!props.roomName) {
+//     } else {
+//       let newRoom = {
+//         Name: props.roomName,
+//       };
+
+//       setDoc(roomRef, newRoom);
+//     }
+//   };
+
+//   roomNameChange = (event) => {
+//     this.setState({
+//       [event.target.name]: event.target.value,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <TextField
+//           name='roomName'
+//           value={this.state.roomName}
+//           onChange={this.roomNameChange}
+//         />
+//         <button onClick={this.joinRoom(this.state.roomName)}>
+//           Join or Create
+//         </button>
+//       </div>
+//     );
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider, ReactReduxContext } from 'react-redux';
 import './style.css';
 import Chat from './components/chat';
+import DrawZone from './components/DrawZone';
 
-ReactDOM.render(<Chat />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <div>
+    <Chat />
+    <DrawZone />
+  </div>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
