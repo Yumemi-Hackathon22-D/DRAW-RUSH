0f8719a902cea264e43031526638962c305b1a55
Naotiki <naotiki@naotiki-apps.xyz>
2022-05-03 19:51:30 +0900

    なぁ、State、お前さ・・・いつになったら更新されんの？みんな心配してたんだぞ・・・あの子はいつ更新されるのかねぇって、State・・・・・・

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,32 +1,91 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useCookies} from 'react-cookie';
 import {firestore, db} from '../firebase/index';
 import {ref, push, set, serverTimestamp, onValue, off} from 'firebase/database';
-import {collection, doc, addDoc, getDoc, onSnapshot} from 'firebase/firestore';
+import {collection, doc, addDoc, getDoc, onSnapshot, updateDoc, deleteDoc} from 'firebase/firestore';
 import {TextField, Button, IconButton, InputAdornment} from '@mui/material';
 import {Send} from '@mui/icons-material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import CheckIcon from '@mui/icons-material/Check'
 
+
+const GameState = {//enum風
+    WAIT_MORE_MEMBER: "waitMember",//独りぼっち　さみしい
+
+    DRAW: "draw",//お絵描き中、画像アップロード待ち
+    CHAT: "chat",//話し合い中
+    CHECK_ANSWER: "checkAnswer",//答え合わせ
+    RESULT: "result",//結果ー＞goto DRAW or END
+
+    END: "end"//ゲーム終了
+}
+
 export const Room = () => {
     const allRoomRef = collection(firestore, 'rooms');
     let roomRef;
     let tmproomName;
+    const [painter, setPainter] = useState('');
     const [isJoined, setIsJoined] = useState(false);
     const [roomName, setroomName] = useState('');
-    const [roomId, setroomId] = useState('');
+    const roomId = useRef('');
+    const userId = useRef('');
     const [messages, setMessages] = useState('');
     const [sendMessage, setSendMessage] = useState('');
     const [userName, setUserName] = useState('');
-    const [userId, setUserID, removeUserID] = useCookies();
+    const [cookie, setCookie, removeCookie] = useCookies();
     const [userDictionary, setUserDictionary] = useState({});
+    const [gameState, setGameState] = useState/*<GameState>*/("");
     const firestoreListenersRef = useRef([]);
     const [isCopied, setIsCopied] = useState(false);
 
+    const SetRoomID = (value) => {
+        roomId.current = value
+        setCookie("roomId", value)
+    }
+    const SetUserId = (value) => {
+        userId.current = value
+        setCookie("userId", value)
+    }
+    useEffect(() => {
+        if (cookie.userId && cookie.roomId) {
+            roomId.current =
+                cookie.roomId
+
+            userId.current = cookie.userId
+            Join()
+        }
+    }, [])
+    //ゲームの進行状態を監視
+    useEffect(() => {
+        console.log(gameState)
+        switch (gameState) {
+            case GameState.WAIT_MORE_MEMBER: {
+                break;
+            }
+            case GameState.DRAW: {
+                break;
+            }
+            case GameState.CHAT: {
+                break;
+            }
+            case GameState.CHECK_ANSWER: {
+                break;
+            }
+            case GameState.RESULT: {
+                break;
+            }
+            case GameState.END: {
+                break;
+            }
+            default :
+                break;
+        }
+    }, [gameState])
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const messageRef = push(ref(db, 'rooms/' + roomId + '/messages/'), {
-            userId: userId.userId,
+        const messageRef = push(ref(db, 'rooms/' + roomId.current + '/messages/'), {
+            userId: userId.current,
             msg: sendMessage,
             timeStamp: serverTimestamp()
         });
@@ -58,79 +117,124 @@ export const Room = () => {
     }
 
     const Join = () => {
-        if (userName === "" || roomName === "") {
-            alert("ルーム名とユーザー名を入力してください")
+        let createSelf = false;
+        if ((userName === "" || roomName === "") && (!cookie.userId && !cookie.roomId)) {
+            alert("ルーム名かユーザー名を入力してください")
             return
         }
         const CheckRoom = async () => {
-            let Ref = await doc(allRoomRef, roomName);
+            let rN = roomName;
+            if (rN === '') {
+                rN = roomId.current
+            }
+            let Ref = await doc(allRoomRef, rN);
             let docSnap = await getDoc(Ref);
             if (!docSnap.exists()) {
                 let Id = await CreateRoom();
-                setroomId(Id)
+                SetRoomID(Id)
                 return Id
             } else {
-                setroomId(roomName)
-                return roomName
+                SetRoomID(rN)
+                return rN
             }
         }
 
 
         const CreateRoom = async () => {
             let res = await addDoc(allRoomRef, {Name: roomName});
-            setroomId(res.id);
+            SetRoomID(res.id)
             await set(ref(db, 'rooms/' + res.id), {messages: ""});
             console.log(res.id);
-            return res.id
+            createSelf = true;
+            return res.id;
         }
         const SetRoom = async (roomId) => {
+
+            console.log(roomId)
             roomRef = await doc(allRoomRef, roomId);
-            if (userId === null || userId === "" || !Object.keys(userId).length) {
+            if (cookie === null || cookie === "" || !Object.keys(cookie).length) {
                 const userRef = await addDoc(collection(roomRef, "/members/"), {
                     name: userName
                 });
 
-                setUserID("userId", userRef.id);
+                SetUserId(userRef.id)
+            }
+            if (createSelf) {
+                await updateDoc(roomRef, {Painter: userId.current});
+                setPainter(userId.current);
             }
 
         }
-
+        const SetGameStateAsync = async (state) => {
+            await updateDoc(roomRef, {State: state}).then(() => {
+                //setGameState(state)
+            });
+        }
         const JoinRoom = async () => {
             let id = await CheckRoom();
             await SetRoom(id);
             return id;
         }
 
-        JoinRoom().then((id) => {
-            const roomDoc = doc(allRoomRef, id)
-            const q = collection(roomDoc, "/members/");
+        JoinRoom()
+            .then((id) => {
+                const roomDoc = doc(allRoomRef, id)//Not
+                const q = collection(roomDoc, "/members/");
 
 
-            firestoreListenersRef.current.push(
-                onSnapshot(q, {
-                    next: (querySnapshot) => {
-                        let tmp = {}
-                        querySnapshot.forEach((doc) => {
+                firestoreListenersRef.current.push(
+                    onSnapshot(q, {
+                        next: (querySnapshot) => {
+                            let tmp = {}
+                            querySnapshot.forEach((doc) => {
 
-                            console.log(doc.id)
-                            console.log(doc.data())
-                            tmp[doc.id] = doc.data().name
-                        });
-                        setUserDictionary(tmp)
-                    }
-                })
-            );
-            firestoreListenersRef.current.push(
-                onSnapshot(roomDoc, {
-                    next: (doc) => {
-                        console.log(doc.data())
-                    }
-                })
-            );
+                                console.log(doc.id)
+                                console.log(doc.data())
+                                tmp[doc.id] = doc.data().name
+                            });
+                            setUserDictionary(tmp)
 
-            JoinChat(id);
-            setIsJoined(true);
-        })
+                            const Alone = async () => {
+                                if (painter !== userId.current) {
+                                    await updateDoc(roomRef, {Painter: userId.current}).then(() => {
+                                        //setPainter(userId.userId)
+                                    });
+                                }
+                                await SetGameStateAsync(GameState.WAIT_MORE_MEMBER);
+                            }
+
+
+                            if (Object.keys(tmp).length <= 1) {
+                                Alone()
+                            } else {
+                                console.log(tmp)
+                                console.log(gameState)
+                                console.log(painter)
+                                console.log(userId.current)
+                                if (gameState === GameState.WAIT_MORE_MEMBER && painter === userId.current) {
+                                    SetGameStateAsync(GameState.DRAW);
+                                }
+
+                            }
+
+
+                        }
+                    })
+                );
+                firestoreListenersRef.current.push(
+                    onSnapshot(roomDoc, {
+                        next: (doc) => {
+                            const data = doc.data()
+                            console.log(data);
+                            setGameState(data.State);
+                            setPainter(data.Painter);
+                        }
+                    })
+                );
+
+                JoinChat(id);
+                setIsJoined(true);
+            })
 
         const JoinChat = (id) => {
             const chatRef = ref(db, 'rooms/' + id + '/messages');
@@ -144,13 +248,17 @@ export const Room = () => {
 
     };
     const Left = () => {
-        const Ref = ref(db, 'rooms/' + roomId + '/messages');
-        off(Ref);
-        firestoreListenersRef.current.forEach((l)=>{
-            l();
+        deleteDoc(doc(allRoomRef, roomId.current + "/members/" + userId.current)).then(() => {
+            const Ref = ref(db, 'rooms/' + roomId.current + '/messages');
+            off(Ref);
+            firestoreListenersRef.current.forEach((l) => {
+                l();
+            });
+
+            removeCookie("userId");
+            removeCookie("roomId");
+            setIsJoined(false);
         });
-        removeUserID("userId");
-        setIsJoined(false);
     }
 
     return (
@@ -195,9 +303,9 @@ export const Room = () => {
                             </InputAdornment>
                     }}></TextField>
                 </div>
-                <div><span>この部屋のID: {roomId}
+                <div><span>この部屋のID: {roomId.current}
                     <IconButton aria-label="copy" onClick={() => {
-                        navigator.clipboard.writeText(roomId)
+                        navigator.clipboard.writeText(roomId.current)
                         Checked();
                     }}> {!isCopied ? <ContentCopyIcon/> : <CheckIcon/>}</IconButton></span></div>
                 <ShowChat></ShowChat></> : <></>}
