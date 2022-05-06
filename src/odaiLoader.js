c4b0896393fd214657024b24c3ea90bf88b3c224
Naotiki <naotiki@naotiki-apps.xyz>
2022-05-06 22:30:37 +0900

    Kotlinを自粛

--- src/odaiLoader.js
+++ src/odaiLoader.js
@@ -3,7 +3,7 @@ function getOdaiFile() {
         method: "GET",
     }).then(response => response.text())
         .then(text => {
-            const arr = text.split(/\r\n|\n/).map(x=>x.split(","));
+            const arr = text.split(/\r\n|\n/).map(x=>x.split(",")).filter(v=>!v.startsWith('//'));
             console.log(arr);
             odais=arr;
         });
